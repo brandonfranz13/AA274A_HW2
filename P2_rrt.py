10666fd26f38134b5472c2db4d05848d0dcfd771
brandonfranz13 <brandonfranz13@gmail.com>
2020-10-04 20:43:08 -0500

    wip

--- P2_rrt.py
+++ P2_rrt.py
@@ -181,9 +181,7 @@ class GeometricRRT(RRT):
         ########## Code starts here ##########
         # Hint: This should take one line.
         d = np.linalg.norm(x1-x2)
-        print(x1.shape)
-        
-        alpha = np.arctan2((x2[:,1]-x1[:,1]), (x1[:,0]-x2[:,0]))
+        alpha = np.arctan2((x2[1]-x1[1]), (x1[0]-x2[0]))
         if d < eps:
             return x2
         else:
