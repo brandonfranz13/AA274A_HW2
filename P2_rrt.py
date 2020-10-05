8f791c2df90fce06361dd17b7269f866de3fce40
brandonfranz13 <brandonfranz13@gmail.com>
2020-10-05 00:35:50 -0500

    wip

--- P2_rrt.py
+++ P2_rrt.py
@@ -255,8 +255,8 @@ class DubinsRRT(RRT):
             return x2
         else:
             x, _ = path_sample(x1, x2, 1.001*self.turning_radius, eps)
-            print(x)
-            return x[1]
+            print(x[1,:])
+            return x[1,:]
         ########## Code ends here ##########
 
     def is_free_motion(self, obstacles, x1, x2, resolution = np.pi/6):
