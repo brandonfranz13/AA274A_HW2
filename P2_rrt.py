45290bda00f455444a08d3af415b977d57a845e5
brandonfranz13 <brandonfranz13@gmail.com>
2020-10-06 11:02:17 -0500

    wip

--- P2_rrt.py
+++ P2_rrt.py
@@ -128,9 +128,9 @@ class RRT(object):
                         # n -=1
                     # import pdb; pdb.set_trace()
                     # self.path = np.squeeze(np.array(path.reverse()))
-                    self.path = np.zeros((len(V[:n,:], state_dim))
+                    self.path = np.zeros((len(V[:n,:]+1, state_dim))
                     self.path[-1,:] = self.x_goal
-                    i = len(V[:n,:] - 1
+                    i = len(V[:n,:]) - 1
                     while i > 0:
                         self.path[i,:] = V[i,:]
                         i = P[i]
