1b637a8a21f8cb6ad2a591b7a842a22a4cdfe7f6
brandonfranz13 <brandonfranz13@gmail.com>
2020-10-05 21:27:50 -0500

    wip

--- P3_traj_planning.py
+++ P3_traj_planning.py
@@ -55,7 +55,7 @@ def compute_smoothed_traj(path, V_des, alpha, dt):
     import pdb; pdb.set_trace()
     path = np.array(path)
     t = np.zeros(len(path))
-    t[1:] = [np.sqrt((path[i,0]-path[i+1,0])**2 + (path[i,1]-path[i+1,1])**2) / V_des for i in range(len(path)-1)])
+    t[1:] = [np.sqrt((path[i,0]-path[i+1,0])**2 + (path[i,1]-path[i+1,1])**2) / V_des for i in range(len(path)-1)]
     t_max = t[-1]
     t_smoothed = np.arange(0.0, t_max, dt)
     sply = scipy.interpolate.splrep(x=t, y=path[:,1], s=alpha)
